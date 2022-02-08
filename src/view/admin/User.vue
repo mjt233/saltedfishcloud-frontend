b58ff396ca7bab6aef333faa118488029a511a43
老咸鱼 <mjt233@qq.com>
2022-02-09 00:29:19 +0800

    feat 优化用户管理中用户表格与分页器的间距

--- src/view/admin/User.vue
+++ src/view/admin/User.vue
@@ -7,7 +7,7 @@
                     <span>用户总数：{{users.total}}，共{{users.pages}}页</span>
                     <pager @change="loadUser" :pageCount="users.pages"></pager>
                 </div>
-                <div class="mdui-table-fluid">
+                <div class="mdui-table-fluid user-table">
                     <table class="mdui-table mdui-table-hoverable" style="min-width: 600px;">
                         <thead>
                             <tr>
@@ -165,6 +165,9 @@ export default {
         margin-right: 12px;
         object-fit: cover;
     }
+}
+.user-table {
+    margin-top: 12px;
 }
 </style>
 
