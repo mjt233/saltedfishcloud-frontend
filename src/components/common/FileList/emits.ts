939f6ae46da65138d836fee7e758fc9d298f1d55
老咸鱼 <mjt233@qq.com>
2022-07-18 23:56:12 +0800

    refactor 移除无用文件

--- src/components/common/FileList/emits.ts
+++
@@ -1,8 +0,0 @@
-import { FileListContext, FileInfo } from '@/core/model'
-
-export interface FileListEmits {
-  (event: 'clickItem', ctx: FileListContext ,item: FileInfo): void,
-  (event: 'back'): void,
-  (event: 'refresh'): void,
-  (event: 'update:file-list', fileList: FileInfo[]): void
-}
