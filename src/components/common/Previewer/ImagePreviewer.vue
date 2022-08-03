f145af2a439bbf8abcb29e0d44517d4c3ef44d7c
老咸鱼 <mjt233@qq.com>
2022-08-04 00:30:04 +0800

    docs 补充ImagePreviewer组件注释

--- src/components/common/Previewer/ImagePreviewer.vue
+++ src/components/common/Previewer/ImagePreviewer.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="image-previewer" :class="{'hid': hid}">
+
+    <!-- 右上角叉叉 -->
     <v-icon
       v-ripple
       class="close-btn"
       icon="mdi-close"
       @click="toClose"
     />
+
+    <!-- 预览条 -->
     <div ref="barRef" class="bar">
+
+      <!-- 预览条自动布局容器 -->
       <grid-container :type="'evenly'" :width="180" style="width: 100%;margin-top: 20px">
         <div
           v-for="(file, index) in fileList"
@@ -31,9 +37,12 @@
         </div>
       </grid-container>
     </div>
+
+    <!-- 主图预览 -->
     <div class="image-container">
       <loading-mask :loading="loading" />
-      <!-- 图片本体 -->
+
+      <!-- 主图 -->
       <v-img v-if="showMainImg" class="main-img" :src="imgSrc">
         <template #placeholder>
           <loading-mask :loading="true" :type="'circular'" />
@@ -47,30 +56,61 @@
 import LoadingMask from '../LoadingMask.vue'
 import FileIcon from '../FileIcon.vue'
 import GridContainer from '@/components/layout/GridContainer.vue'
+// 是否加载中
 const loading = ref(false)
+
+// 当前正在预览的图片下标索引
 const activeIdx = ref(0)
+
+// 是否显示预览主图
 const showMainImg = ref(true)
+
 const props = defineProps({
+  /**
+   * 待预览的图片列表
+   */
   fileList: {
     type: Array as PropType<FileInfo[]>,
     default: () => []
   },
+  /**
+   * 外部指定要预览的图片下标
+   */
   imageIndex: {
     type: Number,
     default: 0
   }
 })
 
+/**
+ * 选择一张图片预览
+ * @param idx 图片下标索引
+ */
 const selectImage = (idx: number) => {
   activeIdx.value = idx
   emits('update:imageIndex', idx)
 }
 
+/**
+ * 监听外部props的imageIndex属性变化，同步更新到activeIdx并触发选择预览操作
+ */
+watch(() => props.imageIndex, () => {
+  if (props.imageIndex != activeIdx.value) {
+    selectImage(props.imageIndex)
+  }
+})
+
+/**
+ * 图片的显示地址
+ */
 const imgSrc = computed(() => {
   const targetFile = props.fileList[activeIdx.value]
   return StringUtils.appendPath(API.getDefaultPrefix(), API.resource.downloadFileByMD5(targetFile.md5, targetFile.name).url)
 })
 
+/**
+ * 监听图片显示地址变化，变化后强制重新渲染v-img组件防止因过渡效果而导致图片无法显示
+ */
 watch(imgSrc, async() => {
   showMainImg.value = false
   await nextTick()
@@ -78,10 +118,21 @@ watch(imgSrc, async() => {
   updateBarScrollTop()
 })
 
+/**
+ * 声明事件
+ */
 const emits = defineEmits(['close', 'update:imageIndex'])
 
+/**
+ * 是否隐藏当前组件（变透明，用于挂载后和关闭前的过渡特效）
+ */
 const hid = ref(false)
+
+/**
+ * 执行关闭当前组件
+ */
 const toClose = () => {
+  // 移除键盘按键监听，并添加隐藏过渡效果，过渡结束后抛出close事件
   window.removeEventListener('keydown', escHandler)
   hid.value = true
   setTimeout(() => {
@@ -89,31 +140,44 @@ const toClose = () => {
   }, 120)
   
 }
+
+/**
+ * 待预览图片缩略图的元素引用
+ */
 const barItemsRef = ref() as Ref<HTMLElement[]>
+
+
+/**
+ * 待预览图片条的元素引用
+ */
 const barRef = ref() as Ref<HTMLElement>
 
-
+/**
+ * 更新待预览图片条的滚动高度，使当前预览的图片位于中间
+ */
 const updateBarScrollTop = () => {
   const thumb = barItemsRef.value[activeIdx.value]
   const bar = barRef.value
   bar.scrollTop = thumb.offsetTop - (bar.clientHeight / 2 - thumb.clientHeight)
 }
 
+/**
+ * 键盘按下Esc按的处理函数
+ * @param e 事件
+ */
 const escHandler = (e: KeyboardEvent) => {
   if (e.key == 'Escape') {
     toClose()
   }
 }
+
+
 onMounted(() => {
+  // 挂载后，添加键盘按键监听，并更新预览下标（监听的移除在toClose中，所以如果需要关闭该组件，应统一调用toClose方法）
   window.addEventListener('keydown', escHandler)
   activeIdx.value = props.imageIndex
 })
 
-watch(() => props.imageIndex, () => {
-  if (props.imageIndex != activeIdx.value) {
-    selectImage(props.imageIndex)
-  }
-})
 </script>
 
 <script lang="ts">
@@ -158,6 +222,7 @@ export default defineComponent({
     opacity: 0;
   }
 
+  // 预览条
   .bar {
     display: flex;
     justify-content: center;
@@ -171,6 +236,8 @@ export default defineComponent({
     &::-webkit-scrollbar-track {
       background: rgb(0, 0, 0);
     }
+    
+    // 预览条中的预览项
     .bar-item {
       display: flex;
       flex-direction: column;
@@ -178,6 +245,8 @@ export default defineComponent({
       margin: 3px 18px;
       align-items: center;
     }
+
+    // 预览条中的图片文件名
     .bar-title {
       width: 100%;
       opacity: .8;
@@ -185,6 +254,7 @@ export default defineComponent({
       transition: all .1s;
     }
 
+    // 预览条中的预览图片容器
     .bar-img-container {
       width: 100%;
       height: 80%;
@@ -203,10 +273,12 @@ export default defineComponent({
     }
   }
 
+  // 预览主图容器
   .image-container {
     width: 100%;
     height: 100%;
     
+    // 预览主图
     .main-img {
       width: 100%;
       height: 100%;
