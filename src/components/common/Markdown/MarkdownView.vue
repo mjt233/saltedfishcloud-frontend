cdce0125939bf8dd71ae6026376241382f3bccd3
xiaotao <mjt233@qq.com>
2023-01-19 02:03:32 +0800

    feat markdown视图添加图片点击看大图模式

--- src/components/common/Markdown/MarkdownView.vue
+++ src/components/common/Markdown/MarkdownView.vue
@@ -31,10 +31,8 @@ const md = new MarkdownIt({
     return `<div class="markdown-code">${result}</div>`
   }
 })
-const html = ref('')
-const update = async() => {
-  html.value = md.render(props.content || '')
-  await nextTick()
+// 渲染a标签，使用md样式
+const renderATag = () => {
   const aTags = rootRef.value.querySelectorAll('a')
   aTags.forEach(el => {
     if(!el.classList.contains('link')) {
@@ -46,6 +44,68 @@ const update = async() => {
     el.target = '_blank'
   })
 }
+
+/**
+ * 给图片添加点击动作，进入看图模式
+ */
+const addImgClickAction = () => {
+  const aTags: NodeListOf<HTMLImageElement> = rootRef.value.querySelectorAll('img')
+  const fileList: FileInfo[] = []
+  // 构造文件列表，并设置图片title
+  aTags.forEach(el => {
+    const url = new URL(el.src)
+    const fileName = ((url.pathname || '').split('/').pop()) as string
+    el.title = fileName
+    fileList.push({
+      name: fileName,
+      path: el.src
+    } as FileInfo)
+  })
+  // 添加点击动作，打开看图模式
+  aTags.forEach((el, idx) => {
+    el.onclick = () => {
+      const inst = SfcUtils.dyncmount(ImagePreviewer, {
+        props: {
+          urlGenerator(file: FileInfo) {
+            return file.path
+          },
+          fileList,
+          thumbnailUrlGenerator(file: FileInfo) {
+            return file.path
+          },
+          imageIndex: idx,
+          style: {
+            position: 'fixed',
+            height: '100vh',
+            width: '100vw',
+            top: 0,
+            left: 0,
+            zIndex: '114514'
+          },
+          onClose() {
+            inst.unmount()
+          },
+        },
+        vappProps: {
+          style: {
+            zIndex: 114514,
+            background: 'none'
+          }
+        },
+        tempDOMHandler(dom) {
+          dom.style.zIndex = '114514'
+        }
+      })
+    }
+  })
+}
+const html = ref('')
+const update = async() => {
+  html.value = md.render(props.content || '')
+  await nextTick()
+  renderATag()
+  addImgClickAction()
+}
 onMounted(update)
 
 watch(() => props.content, update)
@@ -56,6 +116,9 @@ import highlight from 'highlight.js'
 import MarkdownIt from 'markdown-it'
 import 'highlight.js/styles/atom-one-dark.css'
 import { defineComponent, defineProps, defineEmits, Ref, ref, PropType, onMounted, watch, nextTick } from 'vue'
+import SfcUtils from '@/utils/SfcUtils'
+import { ImagePreviewer } from '../Previewer'
+import { FileInfo } from '@/core/model'
 
 export default defineComponent({
   name: 'MarkdownView'
@@ -87,6 +150,7 @@ export default defineComponent({
 
   img {
     max-width: 100%;
+    cursor: pointer;
   }
 
   blockquote {
