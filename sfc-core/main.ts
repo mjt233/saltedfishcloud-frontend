2f5652404c07335d9051a1e310a3ef7af3f7da49
xiaotao <mjt233@qq.com>
2023-03-24 23:14:21 +0800

    feat 开发模式下拓展模块并发导入

--- sfc-core/main.ts
+++ sfc-core/main.ts
@@ -5,10 +5,17 @@ import 'sfc-common/core/boot/globalmount'
 import App from 'sfc-common/App.vue'
 
 
-// 拓展调试用的导入
-if (import.meta.env.DEV) {
-  (import.meta.env.VITE_PLUGINS as string)?.split(',')?.forEach(pluginName => {
-    import(/* @vite-ignore */`../sfc-ext/${import.meta.env.VITE_PLUGIN_PREFIX}-${pluginName}/main`)
-  }) 
+async function start() {
+  // 拓展调试用的导入
+  if (import.meta.env.DEV) {
+    const tasks = [] as Promise<any>[]
+    (import.meta.env.VITE_PLUGINS as string)?.split(',')?.forEach(async pluginName => {
+      const module = `../sfc-ext/${import.meta.env.VITE_PLUGIN_PREFIX}-${pluginName}/main`
+      tasks.push(import(/* @vite-ignore */module))
+    })
+    await Promise.all(tasks)
+  }
+  bootContext.start(buildApp(App))
 }
-bootContext.start(buildApp(App))
+
+start()
