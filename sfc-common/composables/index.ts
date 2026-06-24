99b222d6b6d73db02591a36cabd2bb599649efa0
xiaotao <mjt233@qq.com>
2026-06-24 15:06:26 +0800

    feat: 补充sfc-common模块未导出的通用组合式函数

--- sfc-common/composables/index.ts
+++ sfc-common/composables/index.ts
@@ -1,11 +1,20 @@
+import { useLoadingManager } from './useLoadingManager'
 import { useAutoComputeHeight } from './useAutoComputeHeight'
 import { useCheckIsMobile } from './useCheckIsMobile'
 import { useEventBus } from './useEventBus'
 import { useResizeObserver } from './useResizeObserver'
+import { useDocumentFocus, getCurFocusRootId } from './useDocumentFocus'
+import { useDynamicColCols } from './useDynamicCols'
+import { useTypeToSearch } from './useTypeToSearch'
 
 export {
   useEventBus,
   useCheckIsMobile,
   useResizeObserver,
-  useAutoComputeHeight
+  useAutoComputeHeight,
+  useLoadingManager,
+  useDocumentFocus,
+  getCurFocusRootId,
+  useDynamicColCols,
+  useTypeToSearch
 }
